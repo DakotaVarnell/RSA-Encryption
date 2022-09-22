fe4b587762fbac4f553922dd504f535297be1351
DakotaVarnell <jvarnell1@cub.uca.edu>
2022-09-21 22:27:18 -0500

    added in the input/output

--- RSA.py
+++ RSA.py
@@ -41,14 +41,51 @@ def generate_e(phi_):
 
     return list_of_e
 
+# def print_options():
+#     print("RSA Keys Have Been Generated.")
+#     print("Please Select Your User Type:")
+#     print("\t1. A public user")
+#     print("\t2. The owner of the keys")
+#     print("\t3. Exit program")
+#     print("Enter Your Choice: ", end = '')
 
-def choose_option(choice):
-    if choice is 1:
-        print(str(1))
-    elif choice is 2:
-        print(str(2))
-    elif choice is 3:
-        print(str(3))
+
+# def choose_option():
+#     a = True
+#     while(a):
+#         print("RSA Keys Have Been Generated.")
+#         print("Please Select Your User Type:")
+#         print("\t1. A public user")
+#         print("\t2. The owner of the keys")
+#         print("\t3. Exit program")
+#         print("Enter Your Choice: ", end = '')
+
+#         choice = int(input())
+
+#         match choice:
+#             case 1:
+#                 choice_case_1= int(input())
+#                 print("As a public user, what would you like to do?")
+#                 print("\t1. Send an encrypted message " )
+#                 print("\t2. Authenticate a digital signature ")
+#                 print("\t3. Exit")
+
+#                 match choice_case_1:
+#                     case 1:
+#                         a = False
+#                         return(input("Enter a message: "))
+#                     case 2:
+
+#                     case 3:
+
+
+#             case 2:
+#                 return "two"
+#             case 3:
+#                 return "three"
+#             case default:
+#                 return "something"
+ 
     
 
 #M is the message to encrypt
@@ -66,18 +103,96 @@ candidates = create_candidates()
 #Assign our return variables to p and q as they should be
 
 p, q = fermats_theorem(candidates)
-print('The value of p is: ', p, 'The value of q is: ', q)
 
 N = p*q
 phi = (p-1)*(q-1)
 
 e = generate_e(phi)
-print('e is:', e)
 
 public_key = [N, e]
 
-print("RSA Keys have been Generated")
-print("1. A public user")
-print("2. The owner of the keys") 
-print("3. Exit program")
-choose_option(input())
+#Our frontend begins here
+a = True
+encryption_list = []
+signature_list = []
+signature_validity = True
+
+while(a):
+    print("RSA Keys Have Been Generated.")
+    print("Please Select Your User Type:")
+    print("\t1. A public user")
+    print("\t2. The owner of the keys")
+    print("\t3. Exit program")
+    print("Enter Your Choice: ", end = '')
+
+    choice = int(input())
+
+    match choice:
+        case 1:
+            print("As a public user, what would you like to do?")
+            print("\t1. Send an encrypted message " )
+            print("\t2. Authenticate a digital signature ")
+            print("\t3. Exit")
+            print("Choose One: ", end = "")
+            choice_case_1= int(input())
+
+            b = True
+            
+            while(b):
+                    match choice_case_1:
+                        case 1:
+                            a = False
+                            our_string = (input("Enter a message: "))
+                            #this is where our conversion function needs to be called
+                            #this is where our encryption function needs to be called on our converted string
+                            print("Message encrypted and sent")
+                            b = False
+                        case 2:
+                            if len(signature_list) == 0:
+                                print("There are no signatures to authenticate.")
+                            else:
+                                print("The following messages are available: ")
+                                for i in range(len(signature_list)):
+                                    print(i + ". " + signature_list[i])
+                                    choose_message = int(input())
+                                    
+                                    if signature_validity == True:
+                                        print("Signature is valid.")
+                                    else:
+                                        print("Signature is not valid")
+                            b = False
+                        case 3:
+                            b = False
+        case 2:
+            print("As the owner of the keys, what would you like to do?")
+            print("\t1. Decrypt a received message " )
+            print("\t2. Digitally sign a message ")
+            print("\t3. Exit")
+            print("Choose One: ", end = "")
+            choice_case_2 = int(input())
+
+            c = True
+            
+            while(c):
+                    match choice_case_2:
+                        case 1:
+                            print("The following messages are available")
+
+                            for i in range(len(encryption_list)):
+                                print(i + ". " + "length = " + len(encryption_list[i]))
+                                choose_message = int(input())
+
+                            for i in range(len(encryption_list)):
+                                if choose_message == i:
+                                    #decrypt the message at encryption_list[i]
+                                    print("Decrypted Message: " + "decrypted_message")
+                                    c = False
+                        case 2:
+                            message = input("Enter a message: ")
+                            #sign our message and send it
+                            c = False
+                        case 3:
+                            c = False
+        case 3:
+            print("Bye for Now!")
+
