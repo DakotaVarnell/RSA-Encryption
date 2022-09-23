ffb103dde407c106a871b8e9992032af5bc4f203
DakotaVarnell <jvarnell1@cub.uca.edu>
2022-09-22 20:29:42 -0500

    fixed bugs that I know of for frontend

--- RSA.py
+++ RSA.py
@@ -28,6 +28,80 @@ def fermats_theorem(candidates = []):
     q = primes[1]
     return(p, q)
 
+#creates N
+def createN(p,q):
+    n = p *q
+    return n
+
+#creates what Phi would be
+def createPhi(p,q):
+    phi = (p -1)*(q-1) 
+    return phi
+
+#Euclid's algorithm for finding gcd
+def eagcd(a, b):
+    if b == 0:
+        return a
+    else:
+        return eagcd(b, a % b)
+
+#Euclid's extended algorithm that uses mod inverse
+def eeagcd(a, b):
+    if a == 0:
+            return (b, 0, 1)
+    (g, x1, y1) = eeagcd(b % a, a)
+    x = y1 - (b//a) * x1
+    y = x1
+
+    return g,x,y
+
+#Mod inverse
+def modular_inverse(a,b):
+    g,x,y = eeagcd(a,b)
+    if g != 1:
+        raise Exception("No modular inverse")
+    else: 
+        return x % b    
+
+#Uses eagcd to create a public key
+def create_public_key(phi):
+    e = random.randint(2,phi)
+    i = eagcd(phi,e)
+    while i != 1:
+        e = random.randint(2,phi)
+        i = eagcd(phi, e)
+    return e
+
+#uses eeagcd to create the private key
+def create_Private_Key(phi,e):
+    d = modular_inverse(e,phi)
+    return d
+
+#Simply display a prompt for user input of the message portion of signature
+def getString():
+    Sinput = input("Please type in a string: ")
+    return Sinput
+
+#Will create the signature using the input message and private key
+def creat_signature(Sinput, d, n):
+    Sinput_in_ascii = () #<-- parenthesis is a placeholder, delete when uncomment --> #Sinput_in_numeric(Sinput)
+    signed = pow(Sinput_in_ascii,d,n)
+    print("Signature created succesffuly.")
+    return signed
+
+#Authenticates the digital signature 
+def authenticate_signature(signature, n, e, Sinput_in_ascii):
+    message = pow(signature, e, n)
+    print("Message from signature: ", message)
+    print("Original message: ", Sinput_in_ascii)
+    if message == Sinput_in_ascii:
+        print("Signature is authentic.")
+        return True
+    else:
+        print("Signature is NOT authentic!")
+        return False
+
+
 #the requirements for e are that it must be less than phi and it must be relatively prime to phi
 def generate_e(phi_):
     list_of_e = [] #a list of possible values for e
@@ -42,10 +116,25 @@ def generate_e(phi_):
     return list_of_e
      
 
-#M is the message to encrypt
+#M is the message (in ASCII) to encrypt
 #N and e are the public key
-def encrypt(M, N, e):
-    return pow(M, e, N)
+def encrypt(M, e, N):
+    encryted_message = []
+    for i in M:
+        encryted_message.append(pow(i, e, N))
+
+    return encryted_message
+        
+
+#this takes each individual character and finds its ASCII correspondence
+#it then adds it to the list message_in_ascii
+def message_to_ascii(message_in_char):
+    message_in_ascii = []
+    message_in_char = message_in_char.upper()
+    for i in message_in_char:
+        message_in_ascii.append(ord(i))
+
+    return message_in_ascii
 
 
 #Create our list of possible candidate prime numbers
@@ -64,8 +153,8 @@ public_key = [N, e]
 
 #Our frontend begins here
 a = True
-encryption_list = []
-signature_list = []
+encryption_list = ["String", "String2"]
+signature_list = ["Ss", "Dd"]
 signature_validity = True
 print("RSA Keys Have Been Generated.")
 
@@ -104,46 +193,50 @@ while(a):
                             else:
                                 print("The following messages are available: ")
                                 for i in range(len(signature_list)):
-                                    print(i + ". " + signature_list[i])
-                                    choose_message = int(input())
+                                    print(str(i + 1) + ". " + signature_list[i])
+                                print("Choose One: ", end = "")
+                                choose_message = int(input())
                                     
-                                    if signature_validity == True:
-                                        print("Signature is valid.")
-                                    else:
-                                        print("Signature is not valid")
+                                if signature_validity == True:
+                                    print("Signature is valid.")
+                                else:
+                                    print("Signature is not valid")
                         case 3:
                             b = False
         case 2:
-            print("\nAs the owner of the keys, what would you like to do?")
-            print("\t1. Decrypt a received message " )
-            print("\t2. Digitally sign a message ")
-            print("\t3. Exit")
-            print("Choose One: ", end = "")
-            choice_case_2 = int(input())
 
             c = True
             
             while(c):
-                    match choice_case_2:
-                        case 1:
-                            print("The following messages are available")
+                print("\nAs the owner of the keys, what would you like to do?")
+                print("\t1. Decrypt a received message " )
+                print("\t2. Digitally sign a message ")
+                print("\t3. Exit")
+                print("Choose One: ", end = "")
+                choice_case_2 = int(input())
+                    
+                match choice_case_2:
+                    case 1:
+                        print("\nThe following messages are available")
 
-                            for i in range(len(encryption_list)):
-                                print(i + ". " + "length = " + len(encryption_list[i]))
-                                choose_message = int(input())
+                        for i in range(len(encryption_list)):
+                            print(str(i + 1) + ". " + "length = " + str(len(encryption_list[i])))
+                        
+                        print("Choose One: ", end = "")
+                        choose_message = int(input())
 
-                            for i in range(len(encryption_list)):
-                                if choose_message == i:
-                                    #decrypt the message at encryption_list[i]
-                                    print("Decrypted Message: " + "decrypted_message")
-                            c = False
-                        case 2:
-                            message = input("Enter a message: ")
-                            #sign our message and send it
-                            print("Message signed and sent")
-                            c = False
-                        case 3:
-                            c = False
+                        for i in range(len(encryption_list)):
+                            if choose_message == i + 1:
+                                #decrypt the message at encryption_list[i]
+                                print("Decrypted Message: " + "decrypted_message")
+                        #c = False
+                    case 2:
+                        message = input("Enter a message: ")
+                        #sign our message and send it
+                        print("Message signed and sent")
+                        #c = False
+                    case 3:
+                        c = False
         case 3:
             print("Bye for Now!")
             a = False
